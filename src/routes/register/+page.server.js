a20846c31123d75331bb821547558a76f6180a1b
David Duncan <devduncan89@gmail.com>
2023-11-09 17:29:36 -0500

    registration

---
+++ src/routes/register/+page.server.js
@@ -0,0 +1,49 @@
+import { fail, redirect } from '@sveltejs/kit'
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ locals }) {
+  if (locals?.account) throw redirect(301, '/')
+  return {
+    account: undefined
+  }
+}
+
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+  register: async ({ cookies, request, locals, fetch }) => {
+    const data = await request.formData();
+
+    const username = data.get('username')
+    const email = data.get('email')
+    const password = data.get('password');
+    const confirm_password = data.get('confirm-password');
+
+    if (password !== confirm_password) {
+      return fail(400, { password, matches: false })
+    }
+
+    const body = await JSON.stringify({ username, email, password, confirm_password });
+
+    const registerRequest = await fetch('http://localhost:3001/api/account/register', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body,
+    });
+
+    const registerResult = await registerRequest.json();
+    console.log('register result', registerResult);
+
+    if (registerRequest.ok) {
+      cookies.set('session', registerResult.session.session_id, { httpOnly: true, path: '/' });
+      locals.account = registerResult.account;
+      return {
+        success: true,
+        account: registerResult.account
+      }
+    } else {
+      return fail(400, { username, email, invalid: true })
+    }
+
+  }
+}
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -4,7 +4,11 @@
 
 <h1 class="text-4xl mt-4 text-center capitalize text-blue-100">Sign up</h1>
 
-<form class="flex flex-col max-w-sm w-full mx-auto mt-8 bg-zinc-900 rounded-md mb-8 px-4 py-4" action="POST">
+<form
+	class="flex flex-col max-w-sm w-full mx-auto mt-8 bg-zinc-900 rounded-md mb-8 px-4 py-4"
+	method="POST"
+	action="?/register"
+>
 	<!-- username -->
 	<label for="username">Username:</label>
 	<input type="text" id="username" name="username" />
@@ -20,6 +24,8 @@
 	<!-- confirm password -->
 	<label for="confirm-password">Confirm Password:</label>
 	<input type="password" id="confirm-password" name="confirm-password" autocomplete="new-password" minlength="8" />
+
+	<button type="submit" class="bg-blue-600 h-10 rounded-md mt-6">Create Account</button>
 </form>
 
 <style lang="postcss">
